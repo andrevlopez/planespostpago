cadeb7c5a9a9dbec8f7df8a49714b1c379bfa88b
Cesar Jr Rodriguez <cesarodriguez4@gmail.com>
2017-10-09 16:59:44 -0400

    forms

---
+++ front/js/controllers/nuevoUsuario.js
@@ -0,0 +1,30 @@
+// controller.js
+angular
+.module('app')
+.controller('nuevoUsuario', ['$http', '$state', nuevoUsuario]);
+
+function nuevoUsuario($http, $state) {
+  let vm = this;
+  vm.clear = () => {
+    vm.nombre = '';
+    vm.apellido = '';
+    vm.username = '';
+    vm.email = '';
+    vm.password = '';
+    vm.password2 = '';
+    vm.cargo = '';
+  };
+
+  vm.save = () => {
+    $http.post('/registroUsuario', {
+      usuario: vm.username,
+      password: vm.password,
+      nombre: vm.nombre,
+      apellido: vm.apellido,
+      cargo: vm.cargo,
+      email: vm.email
+    }).then(response => {
+      console.log(response.data);
+    });
+  };
+}
