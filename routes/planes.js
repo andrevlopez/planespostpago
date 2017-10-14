4737f0c5c03d43c3ce768faa21aa151a3df2e7c7
Cesar Jr Rodriguez <cesarodriguez4@gmail.com>
2017-10-14 17:09:45 -0400

    agregando servicios de planes

--- routes/planes.js
+++ routes/planes.js
@@ -12,4 +12,41 @@ module.exports = (app, con) => {
       res.send(row);
     }, true);
   });
+
+  app.get('/planes', (req, res) => {
+    crud.select(con, {
+      select: '*',
+      from: 'planesYtelefonias'
+    }, (err, row) => {
+      if (err) {
+        res.send(err);
+      }
+      res.send(row);
+    }, true );
+  });
+
+  app.delete('/planes', (req, res) => {
+    crud.delete(con, {
+      from: 'planesYtelefonias',
+      where: {id:req.body.id}
+    }, (err, row) => {
+      if (err) {
+        res.send(err);
+      }
+      res.send(row);
+    }, true );
+  });
+
+  app.put('/planes', (req, res) => {
+    crud.update(con, {
+      table: 'planesYtelefonias',
+      where: {id:req.body.id},
+      values: req.body
+    }, (err, row) => {
+      if (err) {
+        res.send(err);
+      }
+      res.send(row);
+    }, true );
+  });
 }
